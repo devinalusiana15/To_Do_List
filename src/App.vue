03229cad99af5a1c1a77da5d52fea55d7d6f2639
devinalusiana15 <devinalusiana15@gmail.com>
2022-10-30 06:01:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,104 @@
+<template>
+    <NavbarVue/>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-bordered mt-5">
+                    <thead>
+                        <tr>
+                            <th> TO DO </th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(item, i) in todoList" :key="i"> 
+                            <td class="align-middle w-75">
+                                {{item.content}}
+                            </td>
+                            <td class="align-middle text-center w-75">
+                                <button class="btn btn-info btn-sm mx-1" @click = "handleEdit(item.id)">
+                                    Edit
+                                </button>
+                                <button class="btn btn-danger btn-sm mx-1" @click = "handleDelete(item.id)">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <td class="align-middle text-center w-75">
+                            <div class="form-goup">
+                                <label for="">{{editMode? "Edit" : "Add"}}</label>
+                            <input type="text" class="form-control" v-model="todoItem.content">
+                            </div>
+                        </td>
+                        <td class="align-middle text-center w-25">
+                            <button class="btn btn-primary btn-sm mx-1" @click = "handleToDoItem">
+                                {{editMode? "Edit" : "Add"}}
+                                </button>
+                                <button 
+                                    v-if="editMode"
+                                    class="btn btn-danger btn-sm mx-1" 
+                                    @click = "handleCancel">
+                                    cancel
+                                </button>
+                        </td>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import NavbarVue from './components/NavbarVue';
+import Axios from "axios";
+const todoUrl = "http://localhost:3500/todo";
+export default {
+    data(){
+        return{
+            todoList: [],
+            todoItem: {},
+            editMode:false,
+        };
+    },
+    components:{
+        NavbarVue : NavbarVue,
+    },
+    methods:{
+        handleEdit(id){
+            this.editMode = true;
+            this.todoItem = this.todoList.find((item)=> item.id == id);
+        },
+        handleCancel(){
+            this.editMode = false;
+            this.todoItem = "";
+        },
+        async handleToDoItem(){
+            const id = this.todoItem.id;
+            if(this.editMode){
+                await Axios.put(`${todoUrl}/${id}`, this.todoItem);
+                this.editMode = false;
+                this.todoItem.content = "";
+            }
+            else
+            {
+                await Axios.post(todoUrl, this.todoItem);
+                this.todoItem.content = "";
+            }
+            Axios.get(todoUrl).then(
+                    (response)=>(this.todoList = response.data)
+                    );
+        },
+        async handleDelete(id){
+            await Axios.delete(`${todoUrl}/${id}`);
+            Axios.get(todoUrl).then(
+                    (response)=>(this.todoList = response.data)
+                    );
+        }
+    },
+    created(){
+            Axios.get(todoUrl).then((response)=>(this.todoList = response.data));
+    },
+}
+</script>
---
+++ src/components/NavbarVue.vue
@@ -0,0 +1,39 @@
+<template style="background-color: #59cfd7;"> 
+    <nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: "NavbarVue",
+  }
+  </script>
+
+  <style>
+  .navbar{
+    background-color: #59cfd7;
+  }
+  </style>
